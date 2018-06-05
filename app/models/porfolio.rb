68ef7d52632702c0f26ee8b13147f467aad53a82
antonmor <ingantonmor@gmail.com>
2018-06-04 23:22:55 -0500

    Implementando concern for manejar rutas de imagenes

--- app/models/porfolio.rb
+++ app/models/porfolio.rb
@@ -1,4 +1,6 @@
 class Porfolio < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -10,11 +12,10 @@ class Porfolio < ApplicationRecord
   after_initialize :set_defaults
 
     def set_defaults #metodo para poner por default la imagen en main y en thumb
-      self.main_image ||="http://via.placeholder.com/600x400"
-      self.thumb_image ||="http://via.placeholder.com/350x150"
-    end
-  if self.main_image == nil
-    self.main_image = "http://via.placeholder.com/600x400"
+           
 
+      self.main_image ||=Placeholder.image_generator(height:'600', width:'400')
+      self.thumb_image ||=Placeholder.image_generator(height:'350', width:'150')
+    end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,12 @@
 class Skill < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :percent_utilized
+
+    after_initialize :set_defaults
+
+    def set_defaults #metodo para poner por default la imagen 
+      self.badge ||=Placeholder.image_generator(height:'250', width:'250')
+    end
+
 end
