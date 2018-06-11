2df4b318f20d42d86f99d2839fadb08ca73552fb
antonmor <ingantonmor@gmail.com>
2018-06-11 10:50:18 -0500

    Implementando configuracion en formulario aplicando atributos anidados

--- app/models/porfolio.rb
+++ app/models/porfolio.rb
@@ -1,7 +1,7 @@
 class Porfolio < ApplicationRecord
   has_many :technologies
   accepts_nested_attributes_for :technologies,  
-                                reject_if: lambda { |attrs| attrs['name'].blank?  }
+                                reject_if: lambda { |attrs| attrs['name'].blank?  } #pasando hashes a un solo campo "name" pero puede ser a varios campos
 
   include Placeholder
 
